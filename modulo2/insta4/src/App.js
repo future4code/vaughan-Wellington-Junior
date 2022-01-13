4681e6af7056556adf7951bfd2679d199e78d341
werruccio <werruccio@gmail.com>
2022-01-13 18:25:52 -0300

    React Renderização de Lista

--- modulo2/insta4/src/App.js
+++ modulo2/insta4/src/App.js
@@ -8,28 +8,79 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
+const PostInput = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;  
+  margin: 10px
+`
 
 class App extends React.Component {
+  state = {
+
+    posts: [
+      {
+        nomeUsuario:"paulinha",
+        fotoUsuario:"https://picsum.photos/50/50",
+        fotoPost:"https://picsum.photos/200/150"
+      },
+      {
+        nomeUsuario:"joaozinho",
+        fotoUsuario:"https://picsum.photos/50/51",
+        fotoPost:"https://picsum.photos/200/151"
+      },
+      {
+        nomeUsuario:"mariazinha",
+        fotoUsuario:"https://picsum.photos/50/52",
+        fotoPost:"https://picsum.photos/200/152"
+      }
+    ],
+
+    valorInputNome:""
+  };
+
+  adicionarPost = () => {
+
+    const novoUser = {
+      nomeUsuario: this.state.valorInputNome,
+      fotoUsuario:"https://picsum.photos/50/53",
+      fotoPost:"https://picsum.photos/200/153"
+    }
+
+    const novoPost = [...this.state.posts, novoUser]
+
+    this.setState({posts: novoPost});
+    this.setState({valorInputNome: ""})
+    
+  };
+
+  onChangeInputNome = (event) => {
+    this.setState({valorInputNome: event.target.value})
+  };
+
   render() {
+    const listaComponentes = this.state.posts.map((pessoa)=> {
+      return (
+        <Post
+          nomeUsuario = {pessoa.nomeUsuario}
+          fotoUsuario = {pessoa.fotoUsuario}
+          fotoPost = {pessoa.fotoPost}
+        />
+      )
+    });
+
     return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        
-        <Post
-          nomeUsuario={'joaozinho'}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
-        
-        <Post
-          nomeUsuario={'mariazinha'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
+       <MainContainer>
+         <PostInput>
+              <input
+                value={this.state.valorInputNome}
+                onChange={this.onChangeInputNome}
+                placeholder={"Nome"}
+                />
+                <button onClick={this.adicionarPost}>Postar</button>
+         </PostInput>
+
+        {listaComponentes}        
       </MainContainer>
     );
   }
